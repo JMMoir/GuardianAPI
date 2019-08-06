b91e37c16154b044ee5399ebf5994c8594652b91
JMMoir <jmmoir@outlook.com>
2019-08-06 16:11:15 +0100

    fetaured article css adjusted

--- src/components/FeaturedArticle.vue
+++ src/components/FeaturedArticle.vue
@@ -1,10 +1,13 @@
 <template>
   <div class = 'feature'>
+    <img :src="article.blocks.main.elements[0].assets[0].file"/>
+    <div>
     <h3>{{article.webTitle}}</h3>
     <!-- <p>{{article.blocks.body[0].bodyTextSummary}}</p> -->
     <p>by: {{article.tags[0].webTitle}}</p>
     <!-- <p>{{article.blocks.main.elements[0].assets[0].file}}</p> -->
-    <img :src="article.blocks.main.elements[0].assets[0].file"/>
+  </div>
+
   </div>
 </template>
 
@@ -21,12 +24,14 @@ export default{
   border-style: solid;
   width: 600px;
   height: 200px;
+  overflow: hidden;
 }
 
 img {
   max-width: 100%;
-    max-height: 100%;
+  max-height: 100%;
   float: left;
+
 }
 
 </style>

9fc3d209078e69c864d927cb0b2ac58b0916bd97
JMMoir <jmmoir@outlook.com>
2019-08-06 14:23:26 +0100

    featured article now displaing some data

---
+++ src/components/FeaturedArticle.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>{{article.webTitle}}</p>
+</template>
+
+<script>
+export default{
+  name: 'FeaturedArticle',
+  props:['article']
+}
+</script>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
     <p>Home</p>
+    <FeaturedArticle :article='this.results[0]'/>
   </div>
 </template>
 
 <script>
+import FeaturedArticle from './FeaturedArticle.vue';
+import StandardArticle from './StandardArticle.vue';
+import SearchBar from './SearchBar.vue';
+
 export default {
   name: 'HomePage',
+  data(){
+    return{
+      results:[]
+    }
+  },
+  components:{
+    FeaturedArticle,
+    StandardArticle,
+    SearchBar
+  },
+
+  mounted(){
+
+    fetch('https://content.guardianapis.com/search?q=debate%20AND%20economy&tag=politics/politics&from-date=2014-01-01&api-key=d682a5ab-feca-4d46-b6c9-8507b8bd6efd')
+    .then(response => response.json())
+    .then((data) => {
+      this.results = data.response.results;
+    })
+  }
 }
 </script>
